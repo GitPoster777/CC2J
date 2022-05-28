ebbb898b0d50656b322773b04d4f76d66a4df5f2
Kenny <103870387+GitPoster777@users.noreply.github.com>
2022-05-28 14:45:04 -0500

    Defining task

--- server/Api.go
+++ server/Api.go
@@ -5,13 +5,19 @@ import(
 )
 type homework struct {
 	ID int `json:"ID"`
-	name string `json:name`
-	content string`json:content`
+	Name string `json:Name`
+	Content string`json:Content`
 }
 
-type Allhomeworks[]homework 
+type Allhomeworks []homework 
 
-
+Allhomeworks{
+	{
+		ID: 1,
+		Name: "First task"
+		Content: "Cont"
+	}
+}
 func main() {
 	fmt.Println("Hello world Champ")
 }
