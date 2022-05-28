35705305060f79966f79bd323c4158357191d7bd
Kenny <103870387+GitPoster777@users.noreply.github.com>
2022-05-28 14:36:19 -0500

    Working in structures

--- server/Api.go
+++ server/Api.go
@@ -5,8 +5,17 @@ import(
 	"net/http"
 )
 type homework struct {
-	
+	ID int `json:"ID"`
+	name string `json:name`
+	content string`json:content`
 }
+
+type Ahomework struct {
+	Clear string `json:Clear`
+
+}
+
+
 func main() {
 	
 }
